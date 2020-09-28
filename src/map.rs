4e60167a4a16fdc282a9d34f72ecb8d1521d77e7
MaulingMonkey <git@maulingmonkey.com>
2020-09-28 15:50:24 -0700

    Map:  Okay, I concede, I need more methods!

--- src/map.rs
+++ src/map.rs
@@ -1,6 +1,6 @@
 use serde::de;
 
-#[cfg_attr(feature = "indexmap", allow(unused_imports))] use std::collections::BTreeMap;
+use std::borrow::Borrow;
 use std::cmp::Ord;
 use std::fmt::{self, Formatter};
 use std::hash::Hash;
@@ -8,8 +8,11 @@ use std::marker::PhantomData;
 
 
 
-#[cfg(not(feature = "indexmap"))] type MapImpl<K, V> = BTreeMap<K, V>;
+#[cfg(not(feature = "indexmap"))] type MapImpl<K, V> = std::collections::BTreeMap<K, V>;
 #[cfg(    feature = "indexmap" )] type MapImpl<K, V> = indexmap::IndexMap<K, V>;
+
+#[cfg(not(feature = "indexmap"))] type Entry<'s, K, V> = std::collections::btree_map::Entry<'s, K, V>;
+#[cfg(    feature = "indexmap" )] type Entry<'s, K, V> = indexmap::map::Entry<'s, K, V>;
 
 #[derive(Clone, Debug)]
 /// A basic un-[Spanned] object/map, with [Spanned] children.
@@ -20,10 +23,22 @@ pub struct Map<K: Hash + Ord, V> {
 }
 
 impl<K: Hash + Ord, V> Map<K, V> {
+    // https://docs.rs/indexmap/1.6.0/indexmap/map/struct.IndexMap.html
+    // https://doc.rust-lang.org/std/collections/struct.BTreeMap.html
+
     #[doc = "Makes a new empty Map."                                                        ] pub fn new() -> Self { Map { map: MapImpl::new() } }
-    #[doc = "Clears the map, removing all elements."                                        ] pub fn clear(&mut self) { self.map.clear() }
-    #[doc = "Inserts a key-value pair into the map, returning the replaced value (if any.)" ] pub fn insert(&mut self, k: K, v: V) -> Option<V> { self.map.insert(k, v) }
     #[doc = "Returns the number of elements in the map."                                    ] pub fn len(&self) -> usize { self.map.len() }
+    #[doc = "`true` if the map contains no elements."                                       ] pub fn is_empty(&self) -> bool { self.map.is_empty() }
+    #[doc = "Clears the map, removing all elements."                                        ] pub fn clear(&mut self) { self.map.clear() }
+    #[doc = "Inserts a key-value pair into the map, returning the replaced value, if any."  ] pub fn insert(&mut self, k: K, v: V) -> Option<V> { self.map.insert(k, v) }
+    #[doc = "Gets the given key's corresponding entry in the map for in-place manipulation."] pub fn entry(&mut self, key: K) -> Entry<K, V> { self.map.entry(key) }
+
+    #[doc = "`true` if the map contains the given key."                                     ] pub fn contains_key   <Q>(&self, key: &Q) -> bool                 where Q: Eq + Ord + Hash + ?Sized, K: Borrow<Q>{ self.map.contains_key(key) }
+    #[doc = "Return a reference to the value stored for key, if any."                       ] pub fn get            <Q>(&self, key: &Q) -> Option<&V>           where Q: Eq + Ord + Hash + ?Sized, K: Borrow<Q>{ self.map.get(key) }
+    #[doc = "Returns the key-value pair corresponding to the supplied key, if any."         ] pub fn get_key_value  <Q>(&self, key: &Q) -> Option<(&K, &V)>     where Q: Eq + Ord + Hash + ?Sized, K: Borrow<Q>{ self.map.get_key_value(key) }
+    #[doc = "Returns a mutable reference to the value stored for key, if any."              ] pub fn get_mut        <Q>(&mut self, key: &Q) -> Option<&mut V>   where Q: Eq + Ord + Hash + ?Sized, K: Borrow<Q>{ self.map.get_mut(key) }
+    #[doc = "Removes a key from the map, returning the previous value, if any."             ] pub fn remove         <Q>(&mut self, key: &Q) -> Option<V>        where Q: Eq + Ord + Hash + ?Sized, K: Borrow<Q>{ self.map.remove(key) }
+    #[doc = "Removes a key from the map, returning the previous key/value pair, if any."    ] pub fn remove_entry   <Q>(&mut self, key: &Q) -> Option<(K, V)>   where Q: Eq + Ord + Hash + ?Sized, K: Borrow<Q>{ self.map.remove_entry(key) }
 
     #[doc = "Gets an iterator over the keys of the map."            ] pub fn keys         (&self)     -> impl Iterator<Item = &K>             { self.map.keys().into_iter() }
     #[doc = "Gets an iterator over the values of the map."          ] pub fn values       (&self)     -> impl Iterator<Item = &V>             { self.map.values().into_iter() }
