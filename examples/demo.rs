1fd30951720a3a200d966f3dc9f32c83e51da0de
MaulingMonkey <git@maulingmonkey.com>
2020-09-29 19:38:26 -0700

    Flesh out examples/demo

--- examples/demo.rs
+++ examples/demo.rs
@@ -1,6 +1,28 @@
-use json_spanned_value as jsv;
+use codespan_reporting::term::{self, termcolor};
+use codespan_reporting::diagnostic::{Diagnostic, Label};
+use codespan_reporting::files::SimpleFiles;
+
+use termcolor::{ColorChoice, StandardStream};
+
+use json_spanned_value::{self as jsv, spanned};
 
 fn main() {
-    let text = "{ \"null\": null, \"string\": \"string\", \"number\": 123, \"array\": [1, 2, 3], \"object\": { \"key\": \"value\" }, \"value\": \"asdf\" }";
-    let _parsed : jsv::Value = jsv::from_str(text).unwrap();
+    let text = include_str!("demo.json");
+    let mut files = SimpleFiles::new();
+    let file = files.add("examples/demo.json", text);
+
+    let example : spanned::Object = jsv::from_str(text).unwrap();
+    for (k,v) in example.iter() {
+        emit(&files, &Diagnostic::note().with_message("This is a key!").with_labels(vec![Label::primary(file, k.range())]));
+        emit(&files, &Diagnostic::note().with_message("This is a value!").with_labels(vec![Label::primary(file, v.range())]));
+    }
+}
+
+fn emit(files: &SimpleFiles<&str, &str>, diag: &Diagnostic<usize>) {
+    term::emit(
+        &mut StandardStream::stdout(ColorChoice::Auto),
+        &term::Config::default(),
+        files,
+        diag
+    ).unwrap();
 }
